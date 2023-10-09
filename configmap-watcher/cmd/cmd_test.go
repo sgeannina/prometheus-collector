bbcdc225511597d38e402278b7a44e027527ea03
Nina Segares <ninasegares@microsoft.com>
2023-10-09 15:44:33 +1300

    Fixed base tests

--- configmap-watcher/cmd/cmd_test.go
+++ configmap-watcher/cmd/cmd_test.go
@@ -1,20 +1,34 @@
 package cmd_test
 
 import (
+	"context"
+	"errors"
 	"go.goms.io/aks/configmap-watcher/cmd"
 	"k8s.io/client-go/kubernetes"
 	testclient "k8s.io/client-go/kubernetes/fake"
 	"testing"
+	"time"
 
 	"github.com/stretchr/testify/assert"
 )
 
-func TestSuccessCommand(t *testing.T) {
+func TestSuccessCommandNoConfigmap(t *testing.T) {
 	var cli cmd.KubeClient = &KubectlMock{}
+	ctx, cancel := context.WithTimeout(context.Background(), 5*time.Second)
+	defer cancel()
+	tmpDir := t.TempDir()
+
 	rootCmd := cmd.NewKubeCommand(&cli)
-	rootCmd.SetArgs([]string{"--kubeconfig-file=/config/fake/kubeconfig", "--settings-volume=/etc/config/settings", "--configmap-name=ama-metrics-settings-configmap", "--configmap-namespace=kube-system"})
-	err := rootCmd.Execute()
-	assert.Nil(t, err)
+	rootCmd.SetArgs([]string{"--kubeconfig-file=/config/fake/kubeconfig", "--settings-volume=" + tmpDir, "--configmap-name=ama-metrics-settings-configmap", "--configmap-namespace=kube-system"})
+
+	go func() {
+		if err := rootCmd.ExecuteContext(ctx); err != nil && !errors.Is(ctx.Err(), context.DeadlineExceeded) {
+			t.Fatalf("Command execution failed: %v", err)
+		}
+	}()
+
+	// Wait for the context to be done (either by command completion or timeout)
+	<-ctx.Done()
 }
 
 func TestInvalidParameter(t *testing.T) {
