8b582f9dee0164dcce58e8f68631a7e68c263a85
Nina Segares <ninasegares@microsoft.com>
2023-10-11 10:36:13 +1300

    More unit test

--- configmap-watcher/cmd/watch_test.go
+++ configmap-watcher/cmd/watch_test.go
@@ -4,6 +4,7 @@ import (
 	"context"
 	"errors"
 	"os"
+	"path"
 	"testing"
 	"time"
 
@@ -18,11 +19,12 @@ func TestWatchForChangesSuccessConfigmapAdded(t *testing.T) {
 	data := loadConfigmapFromFile(t, "../tests/settings-configmap-create.yaml")
 	fakeClient, watch := createFakeClient()
 	// the command runs indefinitely in a loop, therefore we need to cancel it after a while
-	ctx, cancel := context.WithTimeout(context.Background(), 5*time.Second)
+	ctx, cancel := context.WithTimeout(context.Background(), 3*time.Second)
 	defer cancel()
 
 	// Simulate watch event
 	tmpDir := executeConfigmapWatch(ctx, t, fakeClient)
+	data.ResourceVersion = "abc"
 	watch.Add(data)
 	time.Sleep(1 * time.Second)
 
@@ -31,16 +33,74 @@ func TestWatchForChangesSuccessConfigmapAdded(t *testing.T) {
 
 	// Assert result
 	files, _ := os.ReadDir(tmpDir)
-	assert.Equal(t, 9, len(files))
+	checkSettingFilesAreCreated(t, files)
+	version, _ := os.ReadFile(path.Join(tmpDir, "inotifysettingscreated"))
+	assert.Equal(t, data.ResourceVersion, string(version))
+}
+
+func TestWatchForChangesSuccessConfigmapUpdated(t *testing.T) {
+	data := loadConfigmapFromFile(t, "../tests/settings-configmap-create.yaml")
+	fakeClient, watchInterface := createFakeClient()
+	// the command runs indefinitely in a loop, therefore we need to cancel it after a while
+	ctx, cancel := context.WithTimeout(context.Background(), 5*time.Second)
+	defer cancel()
+
+	// Simulate watch event
+	tmpDir := executeConfigmapWatch(ctx, t, fakeClient)
+	data.ResourceVersion = "123"
+	watchInterface.Add(data)
+	time.Sleep(1 * time.Second)
+
+	files, _ := os.ReadDir(tmpDir)
+	checkSettingFilesAreCreated(t, files)
+	version, _ := os.ReadFile(path.Join(tmpDir, "inotifysettingscreated"))
+	assert.Equal(t, "123", string(version))
+
+	dataUpdated := loadConfigmapFromFile(t, "../tests/settings-configmap-update.yaml")
+	dataUpdated.ResourceVersion = "1234"
+	watchInterface.Modify(dataUpdated)
+	time.Sleep(1 * time.Second)
+
+	// Wait for the context to be done
+	<-ctx.Done()
+
+	// Assert final result
+	files, _ = os.ReadDir(tmpDir)
+	checkSettingFilesAreCreated(t, files)
+	version, _ = os.ReadFile(path.Join(tmpDir, "inotifysettingscreated"))
+	assert.Equal(t, "1234", string(version))
+}
+
+func TestWatchForChangesSuccessConfigmapDeleted(t *testing.T) {
+	data := loadConfigmapFromFile(t, "../tests/settings-configmap-create.yaml")
+	fakeClient, watchInterface := createFakeClient()
+	// the command runs indefinitely in a loop, therefore we need to cancel it after a while
+	ctx, cancel := context.WithTimeout(context.Background(), 5*time.Second)
+	defer cancel()
+
+	// Simulate watch event
+	tmpDir := executeConfigmapWatch(ctx, t, fakeClient)
+	data.ResourceVersion = "123"
+	watchInterface.Add(data)
+	time.Sleep(1 * time.Second)
+
+	files, _ := os.ReadDir(tmpDir)
+	checkSettingFilesAreCreated(t, files)
+	version, _ := os.ReadFile(path.Join(tmpDir, "inotifysettingscreated"))
+	assert.Equal(t, "123", string(version))
+
+	watchInterface.Delete(data)
+	time.Sleep(1 * time.Second)
+
+	// Wait for the context to be done
+	<-ctx.Done()
+
+	// Assert final result
+	files, _ = os.ReadDir(tmpDir)
+	assert.Equal(t, 1, len(files))
 	assert.True(t, fileExists(files, "inotifysettingscreated"))
-	assert.True(t, fileExists(files, "default-targets-scrape-interval-settings"))
-	assert.True(t, fileExists(files, "pod-annotation-based-scraping"))
-	assert.True(t, fileExists(files, "prometheus-collector-settings"))
-	assert.True(t, fileExists(files, "schema-version"))
-	assert.True(t, fileExists(files, "config-version"))
-	assert.True(t, fileExists(files, "debug-mode"))
-	assert.True(t, fileExists(files, "default-scrape-settings-enabled"))
-	assert.True(t, fileExists(files, "default-targets-metrics-keep-list"))
+	version, _ = os.ReadFile(path.Join(tmpDir, "inotifysettingscreated"))
+	assert.Equal(t, "", string(version))
 }
 
 func fileExists(files []os.DirEntry, fileName string) bool {
@@ -51,6 +111,19 @@ func fileExists(files []os.DirEntry, fileName string) bool {
 	}
 
 	return false
+}
+
+func checkSettingFilesAreCreated(t *testing.T, files []os.DirEntry) {
+	assert.Equal(t, 9, len(files))
+	assert.True(t, fileExists(files, "inotifysettingscreated"))
+	assert.True(t, fileExists(files, "default-targets-scrape-interval-settings"))
+	assert.True(t, fileExists(files, "pod-annotation-based-scraping"))
+	assert.True(t, fileExists(files, "prometheus-collector-settings"))
+	assert.True(t, fileExists(files, "schema-version"))
+	assert.True(t, fileExists(files, "config-version"))
+	assert.True(t, fileExists(files, "debug-mode"))
+	assert.True(t, fileExists(files, "default-scrape-settings-enabled"))
+	assert.True(t, fileExists(files, "default-targets-metrics-keep-list"))
 }
 
 func executeConfigmapWatch(ctx context.Context, t *testing.T, fakeClient kubernetes.Interface) (settingsVolume string) {
